75b1c092a38e0357afa1afe0eb1f9565a7e0d176
Business-API-Account <iullia_alx@iuliastyle.co.uk>
2023-08-18 20:06:14 +0100

    Add project files.

---
+++ Sharpfile/File_System_Operations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpfile
+{
+    internal interface File_System_Operations
+    {
+        public Task<bool> List_Files();
+        public Task<bool> Create_Directory();
+        public Task<bool> Delete_Directory();
+        public Task<bool> Navigate_To_Next_Directory();
+        public Task<bool> Navigate_To_Previos_Directory();
+        public Task<bool> Search_File();
+        public Task<bool> Create_File();
+        public Task<bool> Delete_File();
+        public Task<bool> Open_File();
+    }
+}
---
+++ Sharpfile/Linux_Based_File_Operations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpfile
+{
+    internal class Linux_Based_File_Operations : File_System_Operations
+    {
+        Task<bool> File_System_Operations.Create_Directory()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> File_System_Operations.Create_File()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> File_System_Operations.Delete_Directory()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> File_System_Operations.Delete_File()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> File_System_Operations.List_Files()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> File_System_Operations.Navigate_To_Next_Directory()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> File_System_Operations.Navigate_To_Previos_Directory()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> File_System_Operations.Open_File()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> File_System_Operations.Search_File()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
